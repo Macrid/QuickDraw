6afa4dcf6b7e4aee351e346eb1f1c20b27a056e7
Erik Persson <erikpersson96@hotmail.com>
2021-02-08 14:45:23 +0100

    Basic email inlogg inlagt

--- app/src/main/java/com/example/quickdraw/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/quickdraw/fragments/LoginFragment.kt
@@ -1,15 +1,21 @@
 package com.example.quickdraw.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import androidx.navigation.Navigation
 import com.example.quickdraw.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginFragment : Fragment() {
+    lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -21,12 +27,22 @@ class LoginFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
-    override fun onStart() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onStart()
+        auth = Firebase.auth
+
         var loginButton = requireView().findViewById<Button>(R.id.loginButton)
+        var emailField = requireView().findViewById<EditText>(R.id.editTextEmailAddress)
+        var passwordField = requireView().findViewById<EditText>(R.id.editTextPassword)
 
         loginButton.setOnClickListener {
-            Navigation.findNavController(requireView()).navigate(R.id.action_loginFragment_to_mainMenuFragment)
+            auth.signInWithEmailAndPassword(emailField.text.toString(), passwordField.text.toString()).addOnCompleteListener {  task ->
+                if (task.isSuccessful) {
+                    Navigation.findNavController(requireView()).navigate(R.id.action_loginFragment_to_mainMenuFragment)
+                } else {
+                    Log.d("DEBUG", "fel inlogg")
+                }
+            }
         }
     }
 }
