79299aa300835d74eddf6103d3ff129f51c78229
Erik Persson <erikpersson96@hotmail.com>
2021-02-17 20:10:03 +0100

    First friendlist search func added

--- app/src/main/java/com/example/quickdraw/fragments/FriendlistFragment.kt
+++ app/src/main/java/com/example/quickdraw/fragments/FriendlistFragment.kt
@@ -1,15 +1,25 @@
 package com.example.quickdraw.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.quickdraw.ActiveGamesAdapter
 import com.example.quickdraw.FriendlistAdapter
 import com.example.quickdraw.R
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
 
 class FriendlistFragment : Fragment() {
 
@@ -30,11 +40,62 @@ class FriendlistFragment : Fragment() {
 
     override fun onStart() {
         super.onStart()
-        var frienlistAdapter = FriendlistAdapter()
-        frienlistAdapter.mainFragment = this
+        var friendlistAdapter = FriendlistAdapter()
+        friendlistAdapter.mainFragment = this
         var friendlistView = requireView().findViewById<RecyclerView>(R.id.friendListRV)
-
         friendlistView.layoutManager = LinearLayoutManager(this.context)
-        friendlistView.adapter = frienlistAdapter
+        friendlistView.adapter = friendlistAdapter
+
+
+        val friendSearch = requireView().findViewById<SearchView>(R.id.friendSearch)
+        val friendSearchListView = requireView().findViewById<ListView>(R.id.friendSearchListView)
+
+        var userDisplayNames = mutableListOf<String>()
+
+        userDisplayNames.add("Test")
+
+        val friendSearchAdapter: ArrayAdapter<String> = ArrayAdapter(requireContext(), R.layout.friend_search_item, userDisplayNames)
+
+        friendSearchListView.adapter = friendSearchAdapter
+
+        val databaseRef = Firebase.database.reference.child("Users")
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (user in dataSnapshot.children)
+                {
+                    Log.w("Debug", user.child("Display Name").value as String)
+                    userDisplayNames.add(user.child("Display Name").value as String)
+                }
+                friendSearchAdapter.notifyDataSetChanged()
+
+                Log.w("Debug", userDisplayNames.count().toString())
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+                Log.w("Debug", databaseError.toException())
+            }
+        }
+        databaseRef.addListenerForSingleValueEvent(postListener)
+
+
+        friendSearch.setOnQueryTextListener(object :SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                friendSearch.clearFocus()
+                if(userDisplayNames.contains(query))
+                {
+                    friendSearchAdapter.filter.filter(query)
+                }
+                else{
+                    Toast.makeText(requireContext().applicationContext, "Found nobody with that name :(", Toast.LENGTH_LONG).show()
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                friendSearchAdapter.filter.filter(newText)
+                return false
+            }
+        })
     }
 }
--- app/src/main/java/com/example/quickdraw/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/quickdraw/fragments/LoginFragment.kt
@@ -9,9 +9,11 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.example.quickdraw.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class LoginFragment : Fragment() {
@@ -21,8 +23,8 @@ class LoginFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
@@ -30,17 +32,31 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onStart()
         auth = Firebase.auth
+        var databaseRef = Firebase.database.reference
 
         var loginButton = requireView().findViewById<Button>(R.id.loginButton)
+        var registerButton = requireView().findViewById<Button>(R.id.registerButton)
         var emailField = requireView().findViewById<EditText>(R.id.editTextEmailAddress)
         var passwordField = requireView().findViewById<EditText>(R.id.editTextPassword)
 
         loginButton.setOnClickListener {
-            auth.signInWithEmailAndPassword(emailField.text.toString(), passwordField.text.toString()).addOnCompleteListener {  task ->
+            auth.signInWithEmailAndPassword(emailField.text.toString(), passwordField.text.toString()).addOnCompleteListener { task ->
                 if (task.isSuccessful) {
                     Navigation.findNavController(requireView()).navigate(R.id.action_loginFragment_to_mainMenuFragment)
                 } else {
                     Log.d("DEBUG", "fel inlogg")
+                }
+            }
+        }
+
+        registerButton.setOnClickListener {
+            auth.createUserWithEmailAndPassword(emailField.text.toString(), passwordField.text.toString()).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Navigation.findNavController(requireView()).navigate(R.id.action_loginFragment_to_mainMenuFragment)
+                    databaseRef.child("Users").child(auth.uid!!).child("Display Name").setValue("Fint namn")
+                    databaseRef.child("Users").child(auth.uid!!).child("ID").setValue(auth.uid!!)
+                } else {
+                    Log.d("DEBUG", "fel reg")
                 }
             }
         }
